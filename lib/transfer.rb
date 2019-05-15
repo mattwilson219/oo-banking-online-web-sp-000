4eafb311687552772230fc1ef38d235de99c025a
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-15 20:32:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,15 @@ class Transfer
       end
     end
 
+    def reverse_transfer
+      if @status == "complete"
+        @sender.balance += @amount
+        @receiver.balance -= @amount 
+        @status = "reversed"
+      end
+     end
+    end
+
 
   end
 
