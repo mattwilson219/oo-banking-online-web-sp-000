d31a1c6bbda8ea715bbc38936a477d8bcaf39600
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-15 20:29:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,9 @@ class Transfer
         @sender.balance -= amount
         @receiver.balance += amount
         @status = "complete"
-
+      else
+        @status = "rejected"
+        puts "Transaction rejected. Please check your account balance."
       end
     end
 
