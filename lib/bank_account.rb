5e62cce32f1d98f1855fdc71cc974d0ba64a3585
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-15 19:53:43 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,17 @@
 class BankAccount
 
-  attr_accessor :name, :balance, :status
+  attr_reader :name 
+  attr_accessor :balance, :status
 
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
   end
+
+  def deposit(amount)
+    @balance += amount
+  end 
+
 
 end
